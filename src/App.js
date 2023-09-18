93d8cd72e81b30193b1787836877bbd4f1c3d9e3
Anargithub <anarkhamzina@gmail.com>
2023-09-18 14:07:43 +0600

    hw6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import ContactCard from "./components/ContactCard/ContactCard";
 
 function App() {
+  // const contacts = [
+  //   {
+  //     avatarUrl: "https://via.placeholder.com/150",
+  //     name: "Jenny",
+  //     email: "jenny@gmail.com",
+  //     age: "25",
+  //   },
+  //   {
+  //     avatarUrl: "https://via.placeholder.com/150",
+  //     name: "Paul",
+  //     email: "paul@gmail.com",
+  //     age: "35",
+  //   },
+  //   {
+  //     avatarUrl: "https://via.placeholder.com/150",
+  //     name: "Peter",
+  //     email: "peter@gmail.com",
+  //     age: "45",
+  //   },
+  // ];
+
+  const [results, setResults] = useState([])
+
+  useEffect(()=>{
+    fetch("https://randomuser.me/api/?results=5")
+    .then(response=>response.json())
+    .then(data=> {
+      setResults(data.results)
+      console.log(data)})
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {results.map((result, index) => {
+        return (
+          <ContactCard key={index}
+            avatarUrl={result.picture.large}
+            name={result.name.first}
+            email={result.email}
+            age={result.dob.age}
+          />
+        );
+      })}
+
     </div>
   );
 }
